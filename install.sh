c653ffa47706da0a3fb724e9e86f0a1604ba2b1d
David Daniel <davydany@gmail.com>
2014-10-04 13:27:28 -0400

    Added support for zsh.

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
+pwd=`pwd`
 
 # setup bash
-ln -s bash/bashrc ~/.bashrc
+ln -sf $pwd/bash/bashrc ~/.bashrc
 
 # setup vim
-ln -s vim/vimrc ~/.vimrc
+ln -sf $pwd/vim/vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # setup zsh
 if which zsh > /dev/null; then
 	curl -L http://install.ohmyz.sh | sh
+	rm ~/.zshrc
+	ln -sf $pwd/zsh/zshrc ~/.zshrc
 else
 	echo "ZSH not installed. Not setting it up. Run this after setting up ZSH."
 fi
